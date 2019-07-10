78ab207fd841ae73e387371cd3ad849daaa777f5
Blake Rosenberg <brosen7@live.spcollege.edu>
2019-07-10 10:14:55 -0400

    Performed some basic refactoring (i.e. removing redundant using directives, removing some commented out code, removing some unused variables that were previously used for testing purposes)

--- InsiderTrades/Views/ListPage.xaml.cs
+++ InsiderTrades/Views/ListPage.xaml.cs
@@ -1,38 +1,19 @@
-﻿using InsiderTrades.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Transactions;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace InsiderTrades.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ListPage : Page
+    public sealed partial class ListPage
     {
         public ListPage(HomePage passedHome)
         {
-            this.InitializeComponent();
-            this.HomePage = passedHome;
-            HomePage.DataContextChanged += (s, e) => this.Bindings.Update();
+            InitializeComponent();
+            HomePage = passedHome;
+            HomePage.DataContextChanged += (s, e) => Bindings.Update();
         }
 
         private HomePage HomePage { get; set; }
-
-
     }
 }
